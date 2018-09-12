4b68677d89a7aa1483d9cc5ddb468c55010c0d1e
StanislawMalanowicz <beben86@wp.pl>
2018-09-12 17:23:06 +0200

    states and collisions

--- js/play.js
+++ js/play.js
@@ -7,26 +7,41 @@
 
     create: function(){
         player = game.add.sprite(100, 100, 'asset')
+        game.physics.enable(player, Phaser.Physics.ARCADE)
+        player.body.bounce.setTo(1,1);
+        player.body.collideWorldBounds = true;
+
+        enemy = game.add.sprite(400, 300, 'asset')
+        game.physics.enable(enemy, Phaser.Physics.ARCADE)
+        enemy.body.bounce.setTo(1,1);
+        enemy.body.collideWorldBounds = true;
+
+
         cursors = game.input.keyboard.createCursorKeys();
 
     },
 
     update: function(){
+        game.physics.arcade.collide(player, enemy)
 
         if(cursors.left.isDown){
-            player.x -=1;
+            player.body.velocity.x -=10;
         }
         if(cursors.right.isDown){
-            player.x +=1;
+            player.body.velocity.x +=10;
+            console.log(player.x)
         }
         if(cursors.down.isDown){
-            player.y +=1;
+            player.body.velocity.y +=10;
         }
         if(cursors.up.isDown){
-            player.y -=1;
+            player.body.velocity.y -=10;
         }
-        // if(this.player.x > 300){
-	    // 	game.state.start('win'); 
+        // if(player.x > 230){
+       
+        //     console.log('jeb')
+       
+        //     game.state.start('win'); 
 	    // }
     }
  }
--- js/win.js
+++ js/win.js
@@ -1,11 +1,14 @@
 var winState = {
     create: function(){
-        game.add.text(100, 100, 'You won! Press space to restart', {fill: '#fff'});
+        game.add.text(100, 100, 'You won! Press space to restart', {fill: '#ff3333'});
 
-        this.spaceKey = game.input.keyboard.addKey(Phaser.keyboard.SPACEBAR);
+        this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+       
         this.spaceKey.onDown.add(this.restartGame, this);
     },
     restartGame: function(){
         game.state.start('menu');
     }
 }
+
+
